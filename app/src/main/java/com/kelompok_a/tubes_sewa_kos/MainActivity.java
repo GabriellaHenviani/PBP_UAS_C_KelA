126d46aa88c355d7026c9ca9d77bfd4b8cf1e022
AldriOctoGaliani <aldrioctogaliani19@gmail.com>
2020-09-30 12:57:34 +0700

    Second Commit

--- app/src/main/java/com/kelompok_a/tubes_sewa_kos/MainActivity.java
+++ app/src/main/java/com/kelompok_a/tubes_sewa_kos/MainActivity.java
@@ -1,8 +1,14 @@
 package com.kelompok_a.tubes_sewa_kos;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //test pull request
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.search:
+                        Toast.makeText(MainActivity.this, "Search", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.booking:
+                        Toast.makeText(MainActivity.this, "Sewa", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.login:
+                        Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                        Toast.makeText(MainActivity.this, "Login", Toast.LENGTH_SHORT).show();
+                        startActivity(intent);
+                        break;
+                }
+                return true;
+            }
+        });
     }
 }
