e2a77e80c1ee66f31cf52574ad119d1de712a1fb
lorddepe <daviddharma34@gmail.com>
2020-10-10 13:21:17 +0700

    layout TambahKosFragment

--- app/src/main/java/com/kelompok_a/tubes_sewa_kos/TambahKosFragment.java
+++ app/src/main/java/com/kelompok_a/tubes_sewa_kos/TambahKosFragment.java
@@ -1,32 +1,28 @@
 package com.kelompok_a.tubes_sewa_kos;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-import com.kelompok_a.tubes_sewa_kos.databinding.FragmentTambahKosBinding;
+import com.google.android.material.textfield.TextInputEditText;
+import com.kelompok_a.tubes_sewa_kos.databinding.ActivityMainBinding;
+
+
+import javax.xml.transform.Result;
 
 public class TambahKosFragment extends Fragment {
 
-    private FragmentTambahKosBinding binding;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_tambah_kos, container, false);
-        View view = binding.getRoot();
-
-        return view;
-    }
 }
