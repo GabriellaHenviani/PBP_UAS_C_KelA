2c5fef6b84482dea0222f5ac9d72b9829e0235fe
AldriOctoGaliani <aldrioctogaliani19@gmail.com>
2020-12-01 12:55:46 +0700

    API user + kost

---
+++ app/src/main/java/com/kelompok_a/tubes_sewa_kos/API/UserAPI.java
@@ -0,0 +1,9 @@
+package com.kelompok_a.tubes_sewa_kos.API;
+
+public class UserAPI {
+    public static final String ROOT_URL   = "https://tubespbpa.000webhostapp.com/";
+    public static final String ROOT_API   = ROOT_URL+ "api/";
+    public static final String URL_SELECT = ROOT_API+"user";
+    public static final String URL_LOGIN = ROOT_API+"login";
+    public static final String URL_REGISTER = ROOT_API+"register";
+}
