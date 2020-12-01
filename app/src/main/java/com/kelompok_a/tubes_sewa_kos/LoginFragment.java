572921080bb6f665109ab0dd9e040f3281a85406
AldriOctoGaliani <aldrioctogaliani19@gmail.com>
2020-12-01 14:10:03 +0700

    Login

--- app/src/main/java/com/kelompok_a/tubes_sewa_kos/LoginFragment.java
+++ app/src/main/java/com/kelompok_a/tubes_sewa_kos/LoginFragment.java
@@ -1,21 +1,45 @@
 package com.kelompok_a.tubes_sewa_kos;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputEditText;
+import com.kelompok_a.tubes_sewa_kos.API.UserAPI;
 import com.kelompok_a.tubes_sewa_kos.databinding.FragmentLoginBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+
+import static com.android.volley.Request.Method.POST;
 
 public class LoginFragment extends Fragment {
 
     private FragmentLoginBinding binding;
     private SharedPref sharedPref;
+    private String emailInput;
+    private String passwordInput;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,18 +77,18 @@ public class LoginFragment extends Fragment {
                 sharedPref.setIsLogin(true);
                 MainActivity.isLogin = true;
 
-                MainActivity.changeMenu(MainActivity.binding.bottomNavigation);
-                Fragment homeFragment = new HomeFragment();
-                getActivity().getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.fragment_layout, homeFragment)
-                        .commit();
+                doLogin(emailInput,passwordInput);
+
+//                MainActivity.changeMenu(MainActivity.binding.bottomNavigation);
+//                Fragment homeFragment = new HomeFragment();
+//                getActivity().getSupportFragmentManager()
+//                        .beginTransaction()
+//                        .replace(R.id.fragment_layout, homeFragment)
+//                        .commit();
             }
         }
 
         public boolean validateLogin() {
-            String emailInput;
-            String passwordInput;
 
             emailInput = binding.inputEmail.getText().toString();
             passwordInput = binding.inputPassword.getText().toString();
@@ -88,5 +112,65 @@ public class LoginFragment extends Fragment {
             Toast.makeText(getActivity(), "Login berhasil", Toast.LENGTH_SHORT).show();
             return true;
         }
+    }
+
+    private void doLogin(final String email,final String pass){
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        StringRequest stringRequest = new StringRequest(POST, UserAPI.URL_LOGIN, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    //Mengubah response string menjadi object
+                    JSONObject obj = new JSONObject(response);
+                    //obj.getString("message") digunakan untuk mengambil pesan message dari response
+                    if(obj.getString("message").equals("Authenticated"))
+                    {
+                        loadFragment(new HomeFragment());
+                        Toast.makeText(getContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getContext(), "Error : " +e.toString(), Toast.LENGTH_SHORT).show();
+                }
+                Toast.makeText(getContext(), "Selesai", Toast.LENGTH_SHORT).show();
+            }
+        }),new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //Disini bagian jika response jaringan terdapat ganguan/error
+                Toast.makeText(getContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams()
+            {
+                /*
+                    Disini adalah proses memasukan/mengirimkan parameter key dengan data value,
+                    dan nama key nya harus sesuai dengan parameter key yang diminta oleh jaringan
+                    API.
+                */
+
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("namaBuku", namaBuku);
+                params.put("pengarang", pengarang);
+                return params;
+            }
+        };
+
+        //Disini proses penambahan request yang sudah kita buat ke reuest queue yang sudah dideklarasi
+        queue.add(stringRequest);
+    }
+
+    public void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if (Build.VERSION.SDK_INT >= 26) {
+            fragmentTransaction.setReorderingAllowed(false);
+        }
+        MainActivity.changeMenu(MainActivity.binding.bottomNavigation);
+        fragmentTransaction.replace(R.id.fragment_layout, fragment)
+                .detach(this)
+                .attach(this)
+                .commit();
     }
 }
